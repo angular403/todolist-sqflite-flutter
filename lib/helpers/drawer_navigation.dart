c43b046affc530c3f7d80a27b24f3d05f30d0bdd
angular403 <angularsprings403@gmail.com>
2021-11-10 15:44:01 +0700

    Aplikasi Todolist menggunakan Flutter Sqflite

---
+++ lib/helpers/drawer_navigation.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertodolistsqfliteapp/screens/categories_screen.dart';
+import 'package:fluttertodolistsqfliteapp/screens/home_screen.dart';
+import 'package:fluttertodolistsqfliteapp/screens/todos_by_category.dart';
+import 'package:fluttertodolistsqfliteapp/services/category_service.dart';
+
+class DrawerNavigaton extends StatefulWidget {
+  @override
+  _DrawerNavigatonState createState() => _DrawerNavigatonState();
+}
+
+class _DrawerNavigatonState extends State<DrawerNavigaton> {
+  List<Widget> _categoryList = List<Widget>();
+
+  CategoryService _categoryService = CategoryService();
+
+  @override
+  initState() {
+    super.initState();
+    getAllCategories();
+  }
+
+  getAllCategories() async {
+    var categories = await _categoryService.readCategories();
+
+    categories.forEach((category) {
+      setState(() {
+        _categoryList.add(InkWell(
+          onTap: () => Navigator.push(
+              context,
+              new MaterialPageRoute(
+                  builder: (context) => new TodosByCategory(category: category['name'],))),
+          child: ListTile(
+            title: Text(category['name']),
+          ),
+        ));
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Drawer(
+        child: ListView(
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: NetworkImage(
+                    'https://i.pinimg.com/originals/82/64/00/826400943f7549d21cec0418d1a32e2b.jpg'),
+              ),
+              accountName: Text('Andrew Wiliam Napitupulu'),
+              accountEmail: Text('andrew404@gmail.com'),
+              decoration: BoxDecoration(color: Colors.blue),
+            ),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text('Home'),
+              onTap: () => Navigator.of(context)
+                  .push(MaterialPageRoute(builder: (context) => HomeScreen())),
+            ),
+            ListTile(
+              leading: Icon(Icons.view_list),
+              title: Text('Categories'),
+              onTap: () => Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) => CategoriesScreen())),
+            ),
+            Divider(),
+            Column(
+              children: _categoryList,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
